0b1695b014905aabd371a7438ef0a29db25b530b
Kevin Darlington <kdarlington@gmail.com>
2014-01-11 13:34:05 -0500

    Added glog formatter.

---
+++ glog_formatter.go
@@ -0,0 +1,77 @@
+package factorlog
+
+import (
+	"bytes"
+	"path/filepath"
+)
+
+type GlogFormatter struct {
+	tmp []byte
+}
+
+func NewGlogFormatter() *GlogFormatter {
+	return &GlogFormatter{make([]byte, 64)}
+}
+
+// This will always be true.
+func (f *GlogFormatter) ShouldRuntimeCaller() bool {
+	return true
+}
+
+// Log lines have this form:
+//         Lmmdd hh:mm:ss.uuuuuu threadid file:line] msg...
+// where the fields are defined as follows:
+//         L                A single character, representing the log level (eg 'I' for INFO)
+//         mm               The month (zero padded; ie May is '05')
+//         dd               The day (zero padded)
+//         hh:mm:ss.uuuuuu  Time in hours, minutes and fractional seconds
+//         threadid         The space-padded thread ID as returned by GetTID()
+//         file             The file name
+//         line             The line number
+//         msg              The user-supplied message
+func (f *GlogFormatter) Format(context LogContext) []byte {
+	res := &bytes.Buffer{}
+
+	file := context.File
+	slash := len(file) - 1
+	for ; slash >= 0; slash-- {
+		if file[slash] == filepath.Separator {
+			break
+		}
+	}
+	if slash >= 0 {
+		file = file[slash+1:]
+	}
+
+	_, month, day := context.Time.Date()
+	hour, minute, second := context.Time.Clock()
+	f.tmp[0] = UcShortestSeverityStrings[context.Severity][0]
+	twoDigits(&f.tmp, 1, int(month))
+	twoDigits(&f.tmp, 3, day)
+	f.tmp[5] = ' '
+	twoDigits(&f.tmp, 6, hour)
+	f.tmp[8] = ':'
+	twoDigits(&f.tmp, 9, minute)
+	f.tmp[11] = ':'
+	twoDigits(&f.tmp, 12, second)
+	f.tmp[14] = '.'
+	nDigits(&f.tmp, 6, 15, context.Time.Nanosecond()/1000)
+	f.tmp[21] = ' '
+	nDigits(&f.tmp, 5, 22, context.Pid)
+	f.tmp[27] = ' '
+	res.Write(f.tmp[:28])
+	res.WriteString(file)
+	f.tmp[0] = ':'
+	n := itoa(&f.tmp, 1, context.Line)
+	f.tmp[n+1] = ']'
+	f.tmp[n+2] = ' '
+	res.Write(f.tmp[:n+3])
+	res.WriteString(context.Message)
+
+	l := len(context.Message)
+	if l > 0 && context.Message[l-1] != '\n' {
+		res.WriteRune('\n')
+	}
+
+	return res.Bytes()
+}
---
+++ glog_formatter_test.go
@@ -0,0 +1,23 @@
+package factorlog
+
+import (
+	"testing"
+)
+
+func TestGlogFormatter(t *testing.T) {
+	f := NewGlogFormatter()
+	expect := "P0108 18:27:14.000123 01234 testing.go:391] hello there!"
+	out := string(f.Format(fmtTestsContext))
+	if expect != out {
+		t.Fatalf("\nexpected: %#v\ngot:      %#v", expect, out)
+	}
+}
+
+func BenchmarkGlogFormatter(b *testing.B) {
+	f := NewGlogFormatter()
+
+	b.ResetTimer()
+	for x := 0; x < b.N; x++ {
+		f.Format(fmtTestsContext)
+	}
+}
